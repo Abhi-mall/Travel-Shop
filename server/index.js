fc4b16e6f22306fff5cf18b402fd3c17c0bce648
Abhishek mall <abhishekmall6391@gmail.com>
2025-05-19 19:26:08 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import userRouter from './routes/user.route.js'
 
 const app = express()
 
-const allowedOrigins = process.env.FRONTEND_URLS.split(',')
+const allowedOrigins = process.env.FRONTEND_URL.split(',')
 
 app.use(cors({
   credentials: true,
